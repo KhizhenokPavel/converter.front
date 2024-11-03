59ef45b591cd1338f491cf1e48c04f139aef3ae7
Khizhenok Pavel <khizhenokp@gmail.com>
2024-11-03 23:07:14 +0300

    Добалена аввторизация через апи ключ

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ import VueAxios from 'vue-axios';
 
 const app = createApp(App);
 
-Axios.defaults.baseURL = 'http://localhost:8080/api';
-Axios.defaults.headers.common['apiKey'] = '2134-3242-2132-6576';
+Axios.defaults.baseURL = 'http://pakhizhenok.site:8080/api';
+Axios.defaults.headers.common['Apikey'] = '2134-3242-2132-6576';
 
 app.use(VueAxios, Axios);
 app.use(createPinia())
